c728e6ae648a6e5026e8b90bed236d329005dd75
Fan <fan@choerodon.io>
2018-06-21 11:45:36 +0800

    [ADD] add java template

---
+++ src/main/java/io/choerodon/demo/DemoServiceApplication.java
@@ -0,0 +1,18 @@
+package io.choerodon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+import io.choerodon.resource.annoation.EnableChoerodonResourceServer;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableEurekaClient
+@EnableChoerodonResourceServer
+public class DemoServiceApplication {
+    public static void main(String[] args){
+        SpringApplication.run(DemoServiceApplication.class, args);
+    }
+}
