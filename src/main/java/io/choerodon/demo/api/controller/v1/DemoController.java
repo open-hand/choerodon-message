4aa561a52b69485e35b5b9fa6719fd534351a100
Fan <fan@choerodon.io>
2018-06-25 14:53:41 +0800

    [FIX] fix controller error

--- src/main/java/io/choerodon/demo/api/controller/v1/DemoController.java
+++ src/main/java/io/choerodon/demo/api/controller/v1/DemoController.java
@@ -4,12 +4,14 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.choerodon.core.iam.ResourceLevel;
 import io.choerodon.swagger.annotation.Permission;
 
-@RestController("/v1/demo")
+@RestController
+@RequestMapping("v1/demo")
 public class DemoController {
 
     @GetMapping("/hello")
