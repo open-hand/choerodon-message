77c840411e2dde3b4ba63d8f25a9942cf2bcab98
flyleft <jcalaz@163.com>
2018-09-13 15:22:06 +0800

    [ADD] websocket连接时添加了token权限校验的功能

--- src/main/java/io/choerodon/notify/websocket/ws/WebSocketHandshakeInterceptor.java
+++ src/main/java/io/choerodon/notify/websocket/ws/WebSocketHandshakeInterceptor.java
@@ -2,6 +2,6 @@ package io.choerodon.notify.websocket.ws;
 
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
-public interface AbstractHandshakeInterceptor extends HandshakeInterceptor {
+public interface WebSocketHandshakeInterceptor extends HandshakeInterceptor {
 
 }
