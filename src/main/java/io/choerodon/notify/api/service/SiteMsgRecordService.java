b9542287cd41224e20704218aff57509d6d6671b
flyleft <jcalaz@163.com>
2018-09-18 15:21:06 +0800

    [IMP] 站内信已读接口和删除接口添加webSocket消息发送

--- src/main/java/io/choerodon/notify/api/service/SiteMsgRecordService.java
+++ src/main/java/io/choerodon/notify/api/service/SiteMsgRecordService.java
@@ -11,23 +11,17 @@ public interface SiteMsgRecordService {
 
     /**
      * 分页查询用户所有未删除消息
-     *
-     * @return
      */
     Page<SiteMsgRecordDTO> pagingQueryByUserId(Long userId, Boolean isRead, PageRequest pageRequest);
 
     /**
      * 批量修改消息为已读
-     *
-     * @param list
      */
     void batchUpdateSiteMsgRecordIsRead(Long userId, Long[] ids);
 
 
     /**
      * 批量修改消息为已删除
-     *
-     * @param list
      */
     void batchUpdateSiteMsgRecordIsDeleted(Long userId, Long[] ids);
 }
