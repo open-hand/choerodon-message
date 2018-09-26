b35fc28436f7914ca750d41088c1dd51a1b3eba6
dengyouquan <youquan.deng@hand-china.com>
2018-09-26 13:23:57 +0800

    [ADD] 增加邮件模板和站内信模板字段约束

--- src/main/java/io/choerodon/notify/api/dto/EmailTemplateDTO.java
+++ src/main/java/io/choerodon/notify/api/dto/EmailTemplateDTO.java
@@ -1,27 +1,42 @@
 package io.choerodon.notify.api.dto;
 
 import io.choerodon.notify.domain.Template;
+import io.swagger.annotations.ApiModelProperty;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.modelmapper.PropertyMap;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class EmailTemplateDTO {
+
+    private static final String CODE_REGULAR_EXPRESSION = "^(?!.*?-$)[a-z][a-z0-9-]*$";
 
     private Long id;
 
+    @ApiModelProperty(value = "模板编码/必填")
     @NotEmpty(message = "error.emailTemplate.codeEmpty")
+    @Size(min = 1, max = 14, message = "error.emailTemplate.code.size")
+    @Pattern(regexp = CODE_REGULAR_EXPRESSION, message = "error.emailTemplate.code.illegal")
     private String code;
 
+    @ApiModelProperty(value = "模板名称/必填")
     @NotEmpty(message = "error.emailTemplate.nameEmpty")
+    @Size(min = 1, max = 30, message = "error.emailTemplate.name.size")
     private String name;
 
+    @ApiModelProperty(value = "是否预定义/非必填")
     private Boolean isPredefined;
 
+    @ApiModelProperty(value = "模板类型/必填")
     @NotEmpty(message = "error.emailTemplate.typeEmpty")
     private String type;
 
+    @ApiModelProperty(value = "模板标题/必填")
     @NotEmpty(message = "error.emailTemplate.titleEmpty")
     private String title;
 
+    @ApiModelProperty(value = "模板内容/必填")
     @NotEmpty(message = "error.emailTemplate.contentEmpty")
     private String content;
 
--- src/main/java/io/choerodon/notify/api/dto/PmTemplateDTO.java
+++ src/main/java/io/choerodon/notify/api/dto/PmTemplateDTO.java
@@ -1,27 +1,42 @@
 package io.choerodon.notify.api.dto;
 
 import io.choerodon.notify.domain.Template;
+import io.swagger.annotations.ApiModelProperty;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.modelmapper.PropertyMap;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class PmTemplateDTO {
+
+    private static final String CODE_REGULAR_EXPRESSION = "^(?!.*?-$)[a-z][a-z0-9-]*$";
 
     private Long id;
 
+    @ApiModelProperty(value = "模板编码/必填")
     @NotEmpty(message = "error.pmTemplate.codeEmpty")
+    @Size(min = 1, max = 14, message = "error.pmTemplate.code.size")
+    @Pattern(regexp = CODE_REGULAR_EXPRESSION, message = "error.pmTemplate.code.illegal")
     private String code;
 
+    @ApiModelProperty(value = "模板名称/必填")
     @NotEmpty(message = "error.pmTemplate.nameEmpty")
+    @Size(min = 1, max = 30, message = "error.pmTemplate.name.size")
     private String name;
 
+    @ApiModelProperty(value = "是否预定义/非必填")
     private Boolean isPredefined;
 
+    @ApiModelProperty(value = "模板类型/必填")
     @NotEmpty(message = "error.pmTemplate.typeEmpty")
     private String type;
 
+    @ApiModelProperty(value = "模板标题/必填")
     @NotEmpty(message = "error.pmTemplate.titleEmpty")
     private String title;
 
+    @ApiModelProperty(value = "模板内容/必填")
     @NotEmpty(message = "error.pmTemplate.contentEmpty")
     private String content;
 
