5e3376c444130efcb3549e3e99b0153f54fc566f
flyleft <jcalaz@163.com>
2018-09-29 10:40:33 +0800

    [IMP] 添加了EtagHeaderFilter

--- src/main/java/io/choerodon/notify/NotifyApplication.java
+++ src/main/java/io/choerodon/notify/NotifyApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
 @EnableFeignClients("io.choerodon")
@@ -14,6 +18,11 @@ public class NotifyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotifyApplication.class, args);
+    }
+
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
     }
 
 }
