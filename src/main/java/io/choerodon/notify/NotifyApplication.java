04aee1da9eee24dbf05910287957f3bcf7fa1a8d
flyleft <jcalaz@163.com>
2018-09-29 11:26:13 +0800

    [REM] 去除了EtagHeaderFilter

--- src/main/java/io/choerodon/notify/NotifyApplication.java
+++ src/main/java/io/choerodon/notify/NotifyApplication.java
@@ -1,14 +1,10 @@
 package io.choerodon.notify;
 
-import io.choerodon.notify.infra.utils.CustomShallowEtagHeaderFilter;
 import io.choerodon.resource.annoation.EnableChoerodonResourceServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-
-import javax.servlet.Filter;
 
 @SpringBootApplication
 @EnableFeignClients("io.choerodon")
@@ -19,11 +15,5 @@ public class NotifyApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotifyApplication.class, args);
     }
-
-    @Bean
-    public Filter shallowEtagHeaderFilter() {
-        return new CustomShallowEtagHeaderFilter();
-    }
-
 }
 
