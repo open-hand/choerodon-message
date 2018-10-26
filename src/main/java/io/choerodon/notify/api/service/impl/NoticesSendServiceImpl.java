dde7087e13dcd1e3ec938ef3848e6054d1a38a42
dengyouquan <youquan.deng@hand-china.com>
2018-10-26 11:48:05 +0800

    [FIX] 修复邮件发送异常时不能发送站内信

--- src/main/java/io/choerodon/notify/api/service/impl/NoticesSendServiceImpl.java
+++ src/main/java/io/choerodon/notify/api/service/impl/NoticesSendServiceImpl.java
@@ -1,5 +1,6 @@
 package io.choerodon.notify.api.service.impl;
 
+import io.choerodon.core.exception.CommonException;
 import io.choerodon.notify.api.dto.EmailConfigDTO;
 import io.choerodon.notify.api.dto.EmailSendDTO;
 import io.choerodon.notify.api.dto.NoticeSendDTO;
@@ -7,6 +8,8 @@ import io.choerodon.notify.api.dto.WsSendDTO;
 import io.choerodon.notify.api.service.EmailSendService;
 import io.choerodon.notify.api.service.NoticesSendService;
 import io.choerodon.notify.api.service.WebSocketSendService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +20,7 @@ import java.util.stream.Collectors;
 
 @Service
 public class NoticesSendServiceImpl implements NoticesSendService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NoticesSendServiceImpl.class);
 
     private EmailSendService emailSendService;
 
@@ -46,9 +50,18 @@ public class NoticesSendServiceImpl implements NoticesSendService {
     @Override
     public void sendNotice(NoticeSendDTO dto) {
         Set<String> emails = dto.getTargetUsers().stream().map(NoticeSendDTO.User::getEmail).collect(Collectors.toSet());
-        emailSendService.sendEmail(dto.getCode(), dto.getParams(), emails);
+        //捕获异常,防止邮件发送失败，影响站内信发送
+        try {
+            emailSendService.sendEmail(dto.getCode(), dto.getParams(), emails);
+        } catch (CommonException e) {
+            LOGGER.info("send email failed!", e);
+        }
         Set<Long> ids = dto.getTargetUsers().stream().map(NoticeSendDTO.User::getId).collect(Collectors.toSet());
-        webSocketSendService.send(dto.getCode(), dto.getParams(), ids,
-                Optional.ofNullable(dto.getFromUser()).map(NoticeSendDTO.User::getId).orElse(null));
+        try {
+            webSocketSendService.send(dto.getCode(), dto.getParams(), ids,
+                    Optional.ofNullable(dto.getFromUser()).map(NoticeSendDTO.User::getId).orElse(null));
+        }catch (CommonException e){
+            LOGGER.info("send station letter failed!", e);
+        }
     }
 }
