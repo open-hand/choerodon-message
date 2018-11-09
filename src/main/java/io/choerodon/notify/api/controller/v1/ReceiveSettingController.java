f554918364d7be76b6b04b9a5aa43061411656ae
dengyouquan <youquan.deng@hand-china.com>
2018-11-09 16:59:44 +0800

    [ADD] add messageType validator;

--- src/main/java/io/choerodon/notify/api/controller/v1/ReceiveSettingController.java
+++ src/main/java/io/choerodon/notify/api/controller/v1/ReceiveSettingController.java
@@ -3,6 +3,7 @@ package io.choerodon.notify.api.controller.v1;
 import io.choerodon.core.oauth.DetailsHelper;
 import io.choerodon.notify.api.dto.ReceiveSettingDTO;
 import io.choerodon.notify.api.service.ReceiveSettingService;
+import io.choerodon.notify.api.validator.CommonValidator;
 import io.choerodon.swagger.annotation.Permission;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -43,18 +44,20 @@ public class ReceiveSettingController {
     @PutMapping
     @Permission(permissionLogin = true)
     @ApiOperation(value = "根据组织或项目或平台或全部更新当前用户接收通知配置")
-    public void updateByUserId(@RequestParam("message_type") String messageType,
+    public void updateByUserId(@ApiParam(name = "消息类型", value = "消息类型", example = "pm/email/sms")
+                               @RequestParam("message_type") String messageType,
                                @ApiParam(name = "源id", value = "如果为空，则表示禁/启用所有通知")
                                @RequestParam(value = "source_id", required = false) Long sourceId,
                                @ApiParam(name = "源类型", value = "如果为空，则表示禁/启用所有通知", example = "site/project/organization")
                                @RequestParam(value = "source_type", required = false) String sourceType,
                                @RequestParam("disable") boolean disable) {
         Long userId = DetailsHelper.getUserDetails().getUserId();
+        CommonValidator.validatorMessageType(messageType);
         if (sourceId == null || sourceType == null) {
             receiveSettingService.updateByUserId(userId, messageType, disable);
             return;
         }
+        CommonValidator.validatorLevel(sourceType);
         receiveSettingService.updateByUserIdAndSourceTypeAndSourceId(userId, sourceType, sourceId, messageType, disable);
-
     }
 }
