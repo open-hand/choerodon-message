acd1de5327476de8ada088bc26b2cac61dfea6b2
Longhe1996 <Longhe1996@foxmail.com>
2019-08-01 18:59:26 +0800

    [ADD]添加消息服务页面相关接口 添加邮件内容的发送设置相关接口

--- src/main/java/io/choerodon/notify/api/service/SendSettingService.java
+++ src/main/java/io/choerodon/notify/api/service/SendSettingService.java
@@ -1,10 +1,8 @@
 package io.choerodon.notify.api.service;
 
 import com.github.pagehelper.PageInfo;
-import io.choerodon.notify.api.dto.BusinessTypeDTO;
-import io.choerodon.notify.api.dto.SendSettingDetailDTO;
-import io.choerodon.notify.api.dto.SendSettingListDTO;
-import io.choerodon.notify.api.dto.SendSettingUpdateDTO;
+import io.choerodon.base.domain.PageRequest;
+import io.choerodon.notify.api.dto.*;
 import io.choerodon.notify.domain.SendSetting;
 import io.choerodon.swagger.notify.NotifyBusinessTypeScanData;
 
@@ -21,7 +19,7 @@ public interface SendSettingService {
                                       String description, String params, int page, int size);
 
     PageInfo<SendSettingListDTO> page(String name, String code,
-                                  String description, String params, int page, int size);
+                                      String description, String params, int page, int size);
 
     SendSetting update(SendSettingUpdateDTO updateDTO);
 
@@ -32,4 +30,70 @@ public interface SendSettingService {
     List<SendSettingDetailDTO> queryByLevelAndAllowConfig(String level, boolean allowConfig);
 
     void delete(Long id);
+
+    /**
+     * 消息服务页
+     * 分页过滤接口
+     *
+     * @param filterDTO   过滤信息
+     * @param params      全局过滤信息
+     * @param pageRequest 分页信息
+     * @return 分页结果
+     */
+    PageInfo<MessageServiceVO> pagingAll(SendSetting filterDTO, String params, PageRequest pageRequest);
+
+    /**
+     * 根据id启用消息服务（对应表；notify_send_setting）
+     *
+     * @param id 记录主键
+     * @return 启用的消息服务信息
+     */
+    MessageServiceVO enabled(Long id);
+
+    /**
+     * 根据id停用消息服务（对应表；notify_send_setting）
+     *
+     * @param id 记录主键
+     * @return 停用的消息服务信息
+     */
+    MessageServiceVO disabled(Long id);
+
+
+    /**
+     * 根据id
+     * 允许配置接受设置
+     * 对应表；notify_send_setting
+     *
+     * @param id 记录主键
+     * @return 消息服务信息
+     */
+    MessageServiceVO allowConfiguration(Long id);
+
+    /**
+     * 根据id
+     * 禁止配置接受设置
+     * 对应表；notify_send_setting
+     *
+     * @param id 记录主键
+     * @return 消息服务信息
+     */
+    MessageServiceVO forbiddenConfiguration(Long id);
+
+
+    /**
+     * 获取邮件内容的发送设置信息
+     *
+     * @param id 发送设置主键
+     * @return 邮件内容的发送设置信息
+     */
+    EmailSendSettingVO getEmailSendSetting(Long id);
+
+
+    /**
+     * 修改邮件内容的发送设置信息
+     *
+     * @param updateVO 更新信息
+     * @return 更新结果
+     */
+    EmailSendSettingVO updateEmailSendSetting(EmailSendSettingVO updateVO);
 }
