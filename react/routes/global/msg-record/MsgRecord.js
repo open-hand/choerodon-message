8abc3f6f9a08d53722735f2a9d5940d543fba4ce
DingZhicheng <zhicheng.ding@hand-china.com>
2019-09-10 14:56:06 +0800

    [IMP]消息日志tab页增加权限

--- react/routes/global/msg-record/MsgRecord.js
+++ react/routes/global/msg-record/MsgRecord.js
@@ -6,8 +6,8 @@ const MsgEmail = asyncRouter(() => import('./msg-email'));
 
 function MsgRecord(props) {
   return (
-    <PageWrap noHeader={['tab1']}>
-      <PageTab title="邮件日志" tabKey="tab1" component={withRouter(MsgEmail)} />
+    <PageWrap noHeader={['choerodon.code.site.message-log']}>
+      <PageTab title="邮件日志" tabKey="choerodon.code.site.message-log" component={withRouter(MsgEmail)} />
       {/* <PageTab title="站内信日志" tabKey="tab2" component={withRouter(MsgEmail)} />    
       <PageTab title="短信日志" tabKey="tab3" component={withRouter(MsgEmail)} />     */}
     </PageWrap>
