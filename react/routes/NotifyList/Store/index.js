6e418398e3f6018106d9344bcac8804a77f5be2d
NollieLeo <736653759@qq.com>
2019-12-05 16:38:41 +0800

    [ADD] 新增优化平台管理->设置->消息服务中的停用弹框操作

--- react/routes/NotifyList/Store/index.js
+++ react/routes/NotifyList/Store/index.js
@@ -35,6 +35,7 @@ export const StoreProvider = withRouter(injectIntl(inject('AppState')(
       queryTreeDataSet,
       currentPageType,
       setCurrentPageType,
+      intl,
     };
     return (
       <Store.Provider value={value}>
