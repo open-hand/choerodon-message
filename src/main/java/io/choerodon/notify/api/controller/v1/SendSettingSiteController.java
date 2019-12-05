ccb3083c40805d30bf0adf7a6de767ea700e4bff
李浩 <310103270@qq.com>
2019-12-05 17:39:31 +0800

    [IMP] receive_setting过滤source_type

--- src/main/java/io/choerodon/notify/api/controller/v1/SendSettingSiteController.java
+++ src/main/java/io/choerodon/notify/api/controller/v1/SendSettingSiteController.java
@@ -68,7 +68,7 @@ public class SendSettingSiteController {
     @GetMapping("/list/allow_config")
     @Permission(permissionLogin = true)
     @ApiOperation(value = "根据层级查询未禁用通知配置的启用状态的发送设置列表")
-    public ResponseEntity<List<SendSettingDetailTreeDTO>> listLevelAndAllowConfig(@RequestParam(name = "source_type", required = false) String level) {
+    public ResponseEntity<List<SendSettingDetailTreeDTO>> listLevelAndAllowConfig(@RequestParam(name = "source_type") String level) {
         return new ResponseEntity<>(sendSettingService.queryByLevelAndAllowConfig(level, true), HttpStatus.OK);
     }
 
