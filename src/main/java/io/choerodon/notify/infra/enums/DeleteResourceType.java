34ee639e79a3d6c0bbd085281e24ffac90d9f590
WangXiang <957038053@qq.com>
2019-12-12 09:22:13 +0800

    [ADD]新增枚举类

---
+++ src/main/java/io/choerodon/notify/infra/enums/DeleteResourceType.java
@@ -0,0 +1,41 @@
+package io.choerodon.notify.infra.enums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * User: Mr.Wang
+ * Date: 2019/12/11
+ */
+public enum DeleteResourceType {
+    INSTANCE("instance"),
+    INGRESS("ingress"),
+    CONFIGMAP("configMap"),
+    CERTIFICATE("certificate"),
+    SECRET("secret"),
+    /**
+     * 删除网络
+     */
+    SERVICE("service");
+
+    private final String typeName;
+
+    DeleteResourceType(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getTypeName() {
+        return this.typeName;
+    }
+
+    public static Map<String, String> nameMapping = new HashMap(6);
+
+    static {
+        nameMapping.put(INSTANCE.getTypeName(), "删除实例");
+        nameMapping.put(INGRESS.getTypeName(), "删除域名");
+        nameMapping.put(SERVICE.getTypeName(), "删除网络");
+        nameMapping.put(CONFIGMAP.getTypeName(), "删除配置映射");
+        nameMapping.put(CERTIFICATE.getTypeName(), "删除证书");
+        nameMapping.put(SERVICE.getTypeName(), "删除密文");
+    }
+}
