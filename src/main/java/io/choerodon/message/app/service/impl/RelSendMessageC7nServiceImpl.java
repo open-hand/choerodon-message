2f5c88370881569bbc4271f3a56a04cf0bce57ff
scp <changping.shi@hand-china.com>
2020-05-13 17:01:23 +0800

    [ADD] 添加发送条件

--- src/main/java/io/choerodon/message/app/service/impl/RelSendMessageC7nServiceImpl.java
+++ src/main/java/io/choerodon/message/app/service/impl/RelSendMessageC7nServiceImpl.java
@@ -1,9 +1,165 @@
 package io.choerodon.message.app.service.impl;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import org.hzero.boot.message.entity.Attachment;
+import org.hzero.boot.message.entity.Message;
+import org.hzero.boot.message.entity.MessageSender;
+import org.hzero.boot.message.entity.Receiver;
+import org.hzero.core.base.BaseConstants;
+import org.hzero.message.app.service.*;
+import org.hzero.message.app.service.impl.RelSendMessageServiceImpl;
+import org.hzero.message.domain.entity.TemplateServer;
+import org.hzero.message.domain.entity.TemplateServerLine;
+import org.hzero.message.domain.repository.TemplateServerLineRepository;
+import org.hzero.message.domain.repository.TemplateServerRepository;
+import org.hzero.message.infra.constant.HmsgConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+
+import io.choerodon.core.enums.MessageAdditionalType;
+import io.choerodon.core.exception.CommonException;
+import io.choerodon.message.app.service.RelSendMessageC7nService;
+import io.choerodon.message.infra.dto.MessageSettingDTO;
+import io.choerodon.message.infra.mapper.MessageSettingC7nMapper;
+import io.choerodon.message.infra.mapper.ReceiveSettingC7nMapper;
+
 /**
  * @author scp
  * @date 2020/5/13
  * @description
  */
-public class RelSendMessageC7nServiceImpl {
+@Service
+public class RelSendMessageC7nServiceImpl extends RelSendMessageServiceImpl implements RelSendMessageC7nService {
+
+    private final WebSendService webSendService;
+    private final EmailSendService emailSendService;
+    private final SmsSendService smsSendService;
+    private final CallSendService callSendService;
+    private final TemplateServerService templateServerService;
+    private final TemplateServerRepository templateServerRepository;
+    private final TemplateServerLineRepository templateServerLineRepository;
+    private final MessageReceiverService messageReceiverService;
+    private final WeChatSendService weChatSendService;
+    private final DingTalkSendService dingTalkSendService;
+    private final ReceiveSettingC7nMapper receiveSettingC7nMapper;
+    private final MessageSettingC7nMapper messageSettingC7nMapper;
+
+    @Autowired
+    public RelSendMessageServiceImpl(WebSendService webSendService,
+                                     EmailSendService emailSendService,
+                                     SmsSendService smsSendService,
+                                     CallSendService callSendService,
+                                     TemplateServerService templateServerService,
+                                     TemplateServerRepository templateServerRepository,
+                                     TemplateServerLineRepository templateServerLineRepository,
+                                     MessageReceiverService messageReceiverService,
+                                     WeChatSendService weChatSendService,
+                                     MessageSettingC7nMapper messageSettingC7nMapper,
+                                     ReceiveSettingC7nMapper receiveSettingC7nMapper,
+                                     DingTalkSendService dingTalkSendService) {
+        this.webSendService = webSendService;
+        this.emailSendService = emailSendService;
+        this.smsSendService = smsSendService;
+        this.callSendService = callSendService;
+        this.templateServerService = templateServerService;
+        this.templateServerRepository = templateServerRepository;
+        this.templateServerLineRepository = templateServerLineRepository;
+        this.messageReceiverService = messageReceiverService;
+        this.weChatSendService = weChatSendService;
+        this.dingTalkSendService = dingTalkSendService;
+        this.receiveSettingC7nMapper = receiveSettingC7nMapper;
+        this.messageSettingC7nMapper = messageSettingC7nMapper;
+    }
+
+    @Override
+    public Map<String, Integer> relSendMessage(MessageSender messageSender) {
+        // 检查接收人列表
+        messageSender = messageReceiverService.queryReceiver(messageSender);
+        TemplateServer templateServer = templateServerService.getTemplateServer(messageSender.getTenantId(), messageSender.getMessageCode());
+        Assert.notNull(templateServer, BaseConstants.ErrorCode.DATA_NOT_EXISTS);
+
+        // 检查用户接收配置
+        messageSender.setReceiverAddressList(filterUserReceiverConfig(messageSender.getReceiverAddressList(),
+                StringUtils.hasText(messageSender.getReceiveConfigCode()) ?
+                        messageSender.getReceiveConfigCode() : templateServer.getMessageCode(), templateServer.getTenantId()));
+        Assert.isTrue(Objects.equals(templateServer.getEnabledFlag(), BaseConstants.Flag.YES), BaseConstants.ErrorCode.DATA_NOT_EXISTS);
+        Map<String, List<TemplateServerLine>> serverLineMap = templateServerLineRepository.enabledTemplateServerLine(templateServer.getTempServerId(), templateServer.getTenantId())
+                .stream().collect(Collectors.groupingBy(TemplateServerLine::getTypeCode));
+
+        return sendMessageC7n(serverLineMap, messageSender, templateServer.getTempServerId());
+    }
+
+    private Map<String, Integer> sendMessageC7n(Map<String, List<TemplateServerLine>> serverLineMap, MessageSender messageSender, Long tempServerId) {
+        Map<String, Integer> result = new HashMap<>(4);
+        List<Receiver> receiverList = messageSender.getReceiverAddressList();
+        Long projectId = (Long) messageSender.getAdditionalInformation().get(MessageAdditionalType.PARAM_PROJECT_ID.getTypeName());
+        Long envId = (Long) messageSender.getAdditionalInformation().get(MessageAdditionalType.PARAM_ENV_ID.getTypeName());
+        String evnetName = (String) messageSender.getAdditionalInformation().get(MessageAdditionalType.PARAM_EVENT_NAME.getTypeName());
+        // 每种类型的模板只能指定一个
+        // 模板允许站内消息 且 (不指定发送方式且userId不为空 或 发送方式中指定了站内消息)
+        if (serverLineMap.containsKey(HmsgConstant.MessageType.WEB) && sendEnable(messageSender.getTypeCodeList(), HmsgConstant.MessageType.WEB)) {
+            receiveFilter(receiverList, tempServerId, projectId);
+            if (!CollectionUtils.isEmpty(receiverList) && projectFilter(messageSender, projectId, envId, evnetName, HmsgConstant.MessageType.WEB)) {
+                sendWeb(serverLineMap, receiverList, messageSender.getTenantId(), messageSender.getLang(), messageSender.getArgs(), result, messageSender.getMessageMap());
+            }
+        }
+        // 模板允许短信 且 (不指定发送方式且phone不为空 或 发送方式中指定了短信)
+        if (serverLineMap.containsKey(HmsgConstant.MessageType.SMS) && sendEnable(messageSender.getTypeCodeList(), HmsgConstant.MessageType.SMS)) {
+            if (projectFilter(messageSender, projectId, envId, evnetName, HmsgConstant.MessageType.SMS)) {
+                sendSms(serverLineMap, receiverList, messageSender.getTenantId(), messageSender.getLang(), messageSender.getArgs(), result, messageSender.getMessageMap());
+            }
+        }
+        // 模板允许邮件 且 (不指定发送方式且email不为空 或 发送方式中指定了邮件)
+        if (serverLineMap.containsKey(HmsgConstant.MessageType.EMAIL) && sendEnable(messageSender.getTypeCodeList(), HmsgConstant.MessageType.EMAIL)) {
+            receiveFilter(receiverList, tempServerId, projectId);
+            sendEmail(serverLineMap, receiverList, messageSender.getTenantId(), messageSender.getLang(), messageSender.getArgs(), result, messageSender.getMessageMap(), messageSender.getAttachmentList(), messageSender.getCcList(), messageSender.getBccList(), messageSender.getBatchSend());
+        }
+        // 模板允许语音 且 (不指定发送方式且phone不为空 或 发送方式中指定了与语音)
+        if (serverLineMap.containsKey(HmsgConstant.MessageType.CALL) && sendEnable(messageSender.getTypeCodeList(), HmsgConstant.MessageType.CALL)) {
+            sendCall(serverLineMap, receiverList, messageSender.getTenantId(), messageSender.getLang(), messageSender.getArgs(), result, messageSender.getMessageMap());
+        }
+        if (result.size() == 0) {
+            throw new CommonException(HmsgConstant.ErrorCode.MISSING_RECIPIENT);
+        }
+        return result;
+    }
+
+
+    /**
+     * 个人接收设置 消息过滤
+     *
+     * @param receiverAddressList
+     * @param tempServerId
+     * @param projectId
+     */
+    private void receiveFilter(List<Receiver> receiverAddressList, Long tempServerId, Long projectId) {
+        List<Long> userIds = receiverAddressList.stream().map(Receiver::getUserId).collect(Collectors.toList());
+        List<Long> removeUserIds = receiveSettingC7nMapper.selectByTemplateServerId(projectId, tempServerId, userIds, HmsgConstant.MessageType.WEB);
+        List<Receiver> removeUserList = receiverAddressList.stream().filter(t -> removeUserIds.contains(t.getUserId())).collect(Collectors.toList());
+        receiverAddressList.removeAll(removeUserList);
+    }
+
+    private Boolean projectFilter(MessageSender messageSender, Long projectId, Long envId, String eventName, String messageType) {
+        MessageSettingDTO messageSettingDTO = messageSettingC7nMapper.selectByParams(projectId, messageSender.getMessageCode(), envId, eventName, messageType);
+        return !ObjectUtils.isEmpty(messageSettingDTO);
+
+    }
+
+    /**
+     * 判断个人接收设置是否启用
+     */
+    private void filterReceiveMessage() {
+
+    }
+
+
 }
--- src/main/java/io/choerodon/message/infra/mapper/ReceiveSettingC7nMapper.java
+++ src/main/java/io/choerodon/message/infra/mapper/ReceiveSettingC7nMapper.java
@@ -1,5 +1,7 @@
 package io.choerodon.message.infra.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
 
 import io.choerodon.message.infra.dto.ReceiveSettingDTO;
@@ -9,7 +11,21 @@ import io.choerodon.mybatis.common.BaseMapper;
  * @author dengyouquan
  **/
 public interface ReceiveSettingC7nMapper extends BaseMapper<ReceiveSettingDTO> {
-    void deleteByUserIdAndSourceTypeAndSourceId(@Param("userId") long userId,
+    void deleteByUserIdAndSourceTypeAndSourceId(@Param("userId") Long userId,
                                                 @Param("sourceType") String sourceType,
                                                 @Param("sourceId") Long sourceId);
+
+    /**
+     * 查询用户接收设置
+     *
+     * @param projectId
+     * @param tempServerId 消息发送设置主表id
+     * @param userIds
+     * @param type      消息发送类型 email/web
+     * @return
+     */
+    List<Long> selectByTemplateServerId(@Param("projectId") Long projectId,
+                                        @Param("tempServerId") Long tempServerId,
+                                        @Param("userIds") List<Long> userIds,
+                                        @Param("type") String type);
 }
