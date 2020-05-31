9a5e69ca3ff16b4eea98f1bde92c85a898b40d7d
zmf <1984654893@qq.com>
2020-05-31 22:02:17 +0800

    [FIX] enable scheduling

--- src/main/java/io/choerodon/message/MessageApplication.java
+++ src/main/java/io/choerodon/message/MessageApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableHZeroMessage
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class MessageApplication {
 
     public static void main(String[] args) {
