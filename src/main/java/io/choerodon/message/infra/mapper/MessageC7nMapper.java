70d968b58a28b0b7eda5b8fd9eab4a0d5b567eed
lihao <310103270@qq.com>
2020-06-04 15:07:16 +0800

    [ADD] 添加邮件查询相关类

--- src/main/java/io/choerodon/message/infra/mapper/MessageC7nMapper.java
+++ src/main/java/io/choerodon/message/infra/mapper/MessageC7nMapper.java
@@ -1,10 +1,11 @@
 package io.choerodon.message.infra.mapper;
 
-import java.util.Date;
-import java.util.List;
-
+import io.choerodon.message.infra.dto.MessageC7nDTO;
 import org.apache.ibatis.annotations.Param;
 import org.hzero.message.domain.entity.Message;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author scp
@@ -14,4 +15,13 @@ import org.hzero.message.domain.entity.Message;
 public interface MessageC7nMapper {
     List<Message> selectEmailMessage(@Param("startDate") Date startDate,
                                      @Param("endDate") Date endDate);
+
+    List<MessageC7nDTO> listMessage(@Param("tenantId") Long tenantId,
+                                    @Param("serverCode") String serverCode,
+                                    @Param("messageTypeCode") String messageTypeCode,
+                                    @Param("subject") String subject,
+                                    @Param("trxStatusCode") String trxStatusCode,
+                                    @Param("startDate") Date startDate,
+                                    @Param("endDate") Date endDate,
+                                    @Param("receiver") String receiver);
 }
