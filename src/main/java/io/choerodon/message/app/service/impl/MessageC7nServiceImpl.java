614a0cffa3377e407d6ce3277e0072d395cbe82d
scp <changping.shi@hand-china.com>
2020-06-04 19:40:10 +0800

    [IMP] 修改邮件查询

--- src/main/java/io/choerodon/message/app/service/impl/MessageC7nServiceImpl.java
+++ src/main/java/io/choerodon/message/app/service/impl/MessageC7nServiceImpl.java
@@ -3,7 +3,12 @@ package io.choerodon.message.app.service.impl;
 import io.choerodon.core.domain.Page;
 import io.choerodon.message.app.service.MessageC7nService;
 import io.choerodon.message.infra.dto.MessageC7nDTO;
+import io.choerodon.message.infra.mapper.MessageC7nMapper;
+import io.choerodon.mybatis.pagehelper.PageHelper;
 import io.choerodon.mybatis.pagehelper.domain.PageRequest;
+
+import org.hzero.message.infra.mapper.MessageMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -13,8 +18,11 @@ import java.util.Date;
  */
 @Service
 public class MessageC7nServiceImpl implements MessageC7nService {
+    @Autowired
+    private MessageC7nMapper messageC7nMapper;
+
     @Override
-    public Page<MessageC7nDTO> listMessage(Long tenantId, String serverCode, String messageTypeCode, String subject, String trxStatusCode, Date startDate, Date endDate, String receiver, PageRequest pageRequest) {
-        return null;
+    public Page<MessageC7nDTO> listMessage(String status, String receiveEmail, String templateType, String failedReason, String params, PageRequest pageRequest) {
+        return PageHelper.doPage(pageRequest, () -> messageC7nMapper.listMessage(status, receiveEmail, templateType, failedReason, params));
     }
 }
