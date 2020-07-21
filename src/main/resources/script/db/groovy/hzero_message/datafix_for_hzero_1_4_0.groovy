6d2665d29bc29121944f55defcda7410ee6bc5e7
王灏 <2507172753@qq.com>
2020-07-22 01:30:54 +0800

    [imp]添加升级hzero1.4.0RELEASE,数据修复groovy

--- src/main/resources/script/db/groovy/hzero_message/datafix_for_hzero_1_4_0.groovy
+++ src/main/resources/script/db/groovy/hzero_message/datafix_for_hzero_1_4_0.groovy
@@ -6,6 +6,26 @@ databaseChangeLog(logicalFilePath: 'script/db/datafix_for_hzero_1_4_0.groovy') {
             UPDATE hmsg_email_filter hef
             SET hef.tenant_id = ( SELECT hes.tenant_id FROM hmsg_email_server hes WHERE hes.server_id = hef.server_id)
             WHERE hef.server_id in ( SELECT server_id FROM hmsg_email_server);
+
+            UPDATE hmsg_message_event hme
+            SET hme.tenant_id = ( SELECT hts.tenant_id FROM hmsg_template_server hts WHERE hts.temp_server_id = hme.temp_server_id)
+            WHERE hme.temp_server_id in ( SELECT temp_server_id FROM hmsg_template_server);
+
+            UPDATE hmsg_receive_config_tl hrct
+            SET hrct.tenant_id = ( SELECT hrc.tenant_id FROM hmsg_receive_config hrc WHERE hrct.receive_id = hrc.receive_id)
+            WHERE hrct.receive_id in ( SELECT receive_id FROM hmsg_receive_config);
+
+            UPDATE hmsg_template_arg hta
+            SET hta.tenant_id = ( SELECT hmt.tenant_id FROM hmsg_message_template hmt WHERE hta.template_id = hmt.template_id)
+            WHERE hta.template_id in ( SELECT template_id FROM hmsg_message_template);
+
+            UPDATE hmsg_template_arg_tl htat
+            SET htat.tenant_id = ( SELECT hta.tenant_id FROM hmsg_template_arg hta WHERE hta.arg_id = htat.arg_id)
+            WHERE htat.arg_id in ( SELECT arg_id FROM hmsg_template_arg);
+
+            UPDATE hmsg_template_server_line htsl
+            SET htsl.tenant_id = ( SELECT hts.tenant_id FROM hmsg_template_server hts WHERE hts.temp_server_id = htsl.temp_server_id)
+            WHERE htsl.temp_server_id in ( SELECT temp_server_id FROM hmsg_template_server);
             """)
     }
 }
