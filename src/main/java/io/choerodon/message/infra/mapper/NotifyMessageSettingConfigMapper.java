a7a3ae654899327fff38daebe00640828487e144
wangxiang <xiangwang04@hand-china.com>
2020-07-24 11:04:11 +0800

    [FIX] 增加配置

---
+++ src/main/java/io/choerodon/message/infra/mapper/NotifyMessageSettingConfigMapper.java
@@ -0,0 +1,10 @@
+package io.choerodon.message.infra.mapper;
+
+import io.choerodon.message.infra.dto.NotifyMessageSettingConfigDTO;
+import io.choerodon.mybatis.common.BaseMapper;
+
+/**
+ * Created by wangxiang on 2020/7/24
+ */
+public interface NotifyMessageSettingConfigMapper extends BaseMapper<NotifyMessageSettingConfigDTO> {
+}
