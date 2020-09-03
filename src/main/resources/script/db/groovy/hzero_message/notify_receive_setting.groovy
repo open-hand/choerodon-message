ed7a7475c58f43c5e925f9006008ba94c80a8079
lihao <310103270@qq.com>
2020-09-03 18:01:18 +0800

    [FIX] 修复数据

--- src/main/resources/script/db/groovy/hzero_message/notify_receive_setting.groovy
+++ src/main/resources/script/db/groovy/hzero_message/notify_receive_setting.groovy
@@ -44,12 +44,21 @@ databaseChangeLog(logicalFilePath: 'script/db/notify_receive_setting.groovy') {
         dropColumn(tableName: 'NOTIFY_RECEIVE_SETTING', columnName: 'IS_DISABLED')
     }
 
-    changeSet(author: 'wangxiang@hand.com', id: '2020-09-03-fixReceiveDate'){
+    changeSet(author: 'wangxiang@hand.com', id: '2020-09-03-fixSendingType'){
         sql("""
              UPDATE notify_receive_setting nrs 
              SET nrs.SENDING_TYPE = 'WEB' 
              WHERE
              nrs.SENDING_TYPE = 'pm'
+        """)
+    }
+
+    changeSet(author: 'lihao', id: '2020-09-03-fixSendingType2') {
+        sql("""
+            UPDATE notify_receive_setting nrs 
+             SET nrs.SENDING_TYPE = 'EMAIL' 
+             WHERE
+             nrs.SENDING_TYPE = 'email'
         """)
     }
 }
