b6df5c311a989a000f07991d02ffd1d84fb785f5
王灏 <2507172753@qq.com>
2020-12-23 15:18:09 +0800

    [IMP] 添加api描述

--- src/main/java/io/choerodon/message/api/controller/v1/C7nProjectMailSendController.java
+++ src/main/java/io/choerodon/message/api/controller/v1/C7nProjectMailSendController.java
@@ -1,5 +1,6 @@
 package io.choerodon.message.api.controller.v1;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import io.choerodon.core.iam.ResourceLevel;
 import io.choerodon.message.api.vo.CustomEmailSendInfoVO;
 import io.choerodon.message.app.service.MessageC7nService;
+import io.choerodon.message.infra.config.C7nSwaggerApiConfig;
 import io.choerodon.swagger.annotation.Permission;
 
 /**
@@ -20,6 +22,7 @@ import io.choerodon.swagger.annotation.Permission;
  * @since 2020/12/15 16:17
  */
 @RestController
+@Api(tags = C7nSwaggerApiConfig.CHOEROODN_USER_MESSAGES)
 @RequestMapping("/choerodon/v1/projects/{project_id}/message/emails")
 public class C7nProjectMailSendController {
 
