ac2bb8ce7bceab2c92906c9c9c5e0f402e8f8122
superlee <superleader8@gmail.com>
2021-03-05 16:12:05 +0800

    [ADD] 添加根据消息通知设置code，查询所有配置了启用的项目的内部接口

--- src/main/java/io/choerodon/message/api/controller/v1/C7nMessageController.java
+++ src/main/java/io/choerodon/message/api/controller/v1/C7nMessageController.java
@@ -1,17 +1,20 @@
 package io.choerodon.message.api.controller.v1;
 
+import io.choerodon.message.api.vo.ProjectMessageVO;
+import io.choerodon.message.app.service.MessageSettingC7nService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import io.choerodon.message.app.service.C7nMessageService;
 import io.choerodon.message.infra.config.C7nSwaggerApiConfig;
 import io.choerodon.swagger.annotation.Permission;
+
+import java.util.List;
 
 /**
  * @author zmf
@@ -23,6 +26,8 @@ import io.choerodon.swagger.annotation.Permission;
 public class C7nMessageController {
     @Autowired
     private C7nMessageService c7nMessageService;
+    @Autowired
+    private MessageSettingC7nService messageSettingC7nService;
 
     @Permission(permissionLogin = true)
     @ApiOperation("彻底删除用户当前的所有站内信")
@@ -30,5 +35,14 @@ public class C7nMessageController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void clearAll() {
         c7nMessageService.deleteAllSiteMessages();
+    }
+
+
+    @PostMapping("/setting/{code}/enabled")
+    @Permission(permissionWithin = true)
+    @ApiModelProperty(value = "根据消息通知设置code，查询所有配置了启用的项目")
+    public ResponseEntity<List<ProjectMessageVO>> listEnabledSettingByCode(@PathVariable(value = "code") String code,
+                                                                           @RequestParam(value = "notify_type") String notifyType) {
+        return ResponseEntity.ok(messageSettingC7nService.listEnabledSettingByCode(code, notifyType));
     }
 }
