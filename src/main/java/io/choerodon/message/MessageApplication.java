eeaf7a22c97c1f7231caf53a1d1ffb720cc6b550
25287 <chihao.ran@hand-china.com>
2021-03-19 15:49:15 +0800

    [ADD]封装批量发送消息接口

--- src/main/java/io/choerodon/message/MessageApplication.java
+++ src/main/java/io/choerodon/message/MessageApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableHZeroMessage
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
+@EnableAsync
 public class MessageApplication {
 
     public static void main(String[] args) {
