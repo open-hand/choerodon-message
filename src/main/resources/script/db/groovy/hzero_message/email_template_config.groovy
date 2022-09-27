de3a873c999a8481977843c6eade7f3f34c12ea2
史常萍 <changping.shi@zknow.com>
2022-09-27 14:55:11 +0800

    [IMP] 修改创建表结构

--- src/main/resources/script/db/groovy/hzero_message/email_template_config.groovy
+++ src/main/resources/script/db/groovy/hzero_message/email_template_config.groovy
@@ -3,7 +3,9 @@ package script.db
 databaseChangeLog(logicalFilePath: 'script/db/email_template_config.groovy') {
     changeSet(author: 'scp', id: '2022-09-26-add-email_template_config') {
         createTable(tableName: "EMAIL_TEMPLATE_CONFIG", remarks: "邮件模板自定义配置") {
-            column(name: 'ID', type: 'BIGINT UNSIGNED', autoIncrement: true, remarks: '表ID，主键，供其他表做外键，unsigned bigint、单表时自增、步长为 1')
+            column(name: 'ID', type: 'BIGINT UNSIGNED', autoIncrement: true, remarks: '表ID，主键，供其他表做外键，unsigned bigint、单表时自增、步长为 1'){
+                constraints(primaryKey: true, primaryKeyName: 'PK_EMAIL_TEMPLATE_CONFIG')
+            }
             column(name: 'LOGO', type: 'text', remarks: '邮件logo')
             column(name: 'SLOGAN', type: 'VARCHAR(32)', remarks: '邮件标语')
             column(name: 'FOOTER', type: 'text', remarks: '邮件页脚')
